8a24ca3f96c4e4096d413a9846cc58331e05a3e9
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2024-01-27 00:12:34 +0700

    i

--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,8 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <div className="h-[90vh]">
+      <div className="h-[86vh]">
         <Header />
-        {/* {Feed} */}
-        {/* Subreddit info */}
-        {/* <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
-          <div className="bg-emerald-600 px-6 py-4">
-            <p className="font-semibold py-3 flex items-center gap-1.5">
-              <HomeIcon />
-              Home
-            </p>
-          </div>
-        </div> */}
-
       </div>
     </div>
   );
