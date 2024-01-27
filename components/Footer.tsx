2579d83b46bd015b1cd3a39952854e3f81e15f0f
Faiz Firdaus <faiz.firdaus.2004@gmail.com>
2024-01-27 14:31:19 +0700

    i

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return ( 
+        <div className="flex justify-center text-zinc-200 py-5 relative">
+            <p className="text-center">
+            Created By Faiz Firdaus - 2024
+
+            </p>
+        </div>
+     );
+}
+ 
+export default Footer;
